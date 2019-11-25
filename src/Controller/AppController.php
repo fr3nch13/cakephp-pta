be952d6133ebc0f4ab743335f93b9392e37ef0f8
Brian French <brian.french@nih.gov>
2019-11-25 18:46:35 -0500

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,9 +1,15 @@
 <?php
+/**
+ * AppController
+ */
 
 namespace Fr3nch13\Pta\Controller;
 
 use App\Controller\AppController as BaseController;
 
+/**
+ * App Controller
+ */
 class AppController extends BaseController
 {
 }
