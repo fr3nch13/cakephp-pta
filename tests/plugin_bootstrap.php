76d174e5ae9aa21cabb9e10f2960e9715613fe5e
Brian French <brian.french@nih.gov>
2020-01-03 15:45:00 -0500

    Finally got a proper setup.

--- tests/plugin_bootstrap.php
+++ tests/plugin_bootstrap.php
@@ -1,38 +1,46 @@
 <?php
 // @codingStandardsIgnoreFile
 
+use Cake\Cache\Cache;
+use Cake\Console\ConsoleErrorHandler;
 use Cake\Core\Configure;
+use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Datasource\ConnectionManager;
+use Cake\Error\ErrorHandler;
+use Cake\Log\Log;
+use Cake\Mailer\Email;
+use Cake\Mailer\TransportFactory;
 use Cake\Routing\DispatcherFactory;
 use Cake\Filesystem\Folder;
-use Croogo\Core\Test\Fixture\SettingsFixture;
+use Cake\Utility\Security;
 
-$findVendor = function () {
-    $root = dirname(__DIR__);
+$findRoot = function ($root) {
     if (is_dir($root . '/vendor/cakephp/cakephp')) {
-        return $root . DS. 'vendor' . DS;
+        return $root;
     }
-
-    $root = dirname(dirname(dirname(dirname(__DIR__))));
-    if (is_dir($root . '/vendor/cakephp/cakephp')) {
-        return $root . DS. 'vendor' . DS;
-    }
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
+    throw new Exception("Cannot find the root of the application, unable to run tests");
 };
+
+if (!defined('PLUGIN_ROOT')) {
+    define('PLUGIN_ROOT', $findRoot(getcwd()));
+}
 
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
 
-define('VENDOR', $findVendor());
-
 /**
  * Configure paths required to find CakePHP + general filepath
  * constants
  */
-require dirname(__DIR__) . DS . 'tests' . DS . 'test_app' . DS . 'config' . DS . '/paths.php';
-
-// Use composer to load the autoloader.
-require VENDOR . 'autoload.php';
+require PLUGIN_ROOT . DS . 'tests' . DS . 'test_app' . DS . 'config' . DS . '/paths.php';
 
 /**
  * Bootstrap CakePHP.
@@ -45,16 +53,65 @@ require VENDOR . 'autoload.php';
  */
 require CORE_PATH . 'config' . DS . 'bootstrap.php';
 
+// Use composer to load the autoloader.
+require PLUGIN_ROOT . DS . 'vendor' . DS . 'autoload.php';
+
+/*
+ * Read configuration file and inject configuration into various
+ * CakePHP classes.
+ *
+ * By default there is only one configuration file. It is often a good
+ * idea to create multiple configuration files, and separate the configuration
+ * that changes from configuration that does not. This makes deployment simpler.
+ */
+try {
+    Configure::config('default', new PhpConfig());
+    Configure::load('app', 'default', false);
+} catch (\Exception $e) {
+    exit($e->getMessage() . "\n");
+}
+
+/*
+ * When debug = true the metadata cache should only last
+ * for a short time.
+ */
+if (Configure::read('debug')) {
+    Configure::write('Cache._cake_model_.duration', '+2 minutes');
+    Configure::write('Cache._cake_core_.duration', '+2 minutes');
+}
+
 date_default_timezone_set('UTC');
 
-Configure::write('App', [
-    'namespace' => 'App',
-    'paths' => [
-        'plugins' => [ROOT . DS . 'plugins' . DS],
-        'templates' => [APP . 'Template' . DS],
-        'locales' => [APP . 'Locale' . DS],
-    ]
-]);
+/*
+ * Register application error and exception handlers.
+ */
+$isCli = PHP_SAPI === 'cli';
+if ($isCli) {
+    (new ConsoleErrorHandler(Configure::read('Error')))->register();
+} else {
+    (new ErrorHandler(Configure::read('Error')))->register();
+}
+
+
+/*
+ * Include the CLI bootstrap overrides.
+ */
+if ($isCli) {
+    /**
+     * Additional bootstrapping and configuration for CLI environments should
+     * be put here.
+     */
+
+    // Commented out as it is set above this.
+    // Set the fullBaseUrl to allow URLs to be generated in shell tasks.
+    // This is useful when sending email from shells.
+    //Configure::write('App.fullBaseUrl', php_uname('n'));
+
+    // Set logs to different files so they don't have permission conflicts.
+    Configure::write('Log.debug.file', 'cli-debug');
+    Configure::write('Log.error.file', 'cli-error');
+}
+
 Configure::write('debug', true);
 
 $tmpDirectory = new Folder(TMP);
@@ -63,33 +120,13 @@ $tmpDirectory->create(TMP . 'cache/models', 0777);
 $tmpDirectory->create(TMP . 'cache/persistent', 0777);
 $tmpDirectory->create(TMP . 'cache/views', 0777);
 
-$cache = [
-    'default' => [
-        'engine' => 'File'
-    ],
-    '_cake_core_' => [
-        'className' => 'File',
-        'prefix' => 'croogo_core_myapp_cake_core_',
-        'path' => CACHE . 'persistent/',
-        'serialize' => true,
-        'duration' => '+10 seconds'
-    ],
-    '_cake_model_' => [
-        'className' => 'File',
-        'prefix' => 'croogo_core_my_app_cake_model_',
-        'path' => CACHE . 'models/',
-        'serialize' => 'File',
-        'duration' => '+10 seconds'
-    ],
-    'cached_settings' => [
-        'engine' => 'File',
-        'groups' => ['settings'],
-    ],
-];
-Cake\Cache\Cache::setConfig($cache);
-Configure::write('Session', [
-    'defaults' => 'php'
-]);
+Cache::setConfig(Configure::consume('Cache'));
+
+//ConnectionManager::setConfig(Configure::consume('Datasources'));
+TransportFactory::setConfig(Configure::consume('EmailTransport'));
+Email::setConfig(Configure::consume('Email'));
+Log::setConfig(Configure::consume('Log'));
+Security::setSalt(Configure::consume('Security.salt'));
 
 // Ensure default test connection is defined
 if (!getenv('db_class')) {
@@ -116,7 +153,3 @@ ConnectionManager::setConfig('test_migrations', [
 ]);
 
 ConnectionManager::alias('test', 'default');
-Configure::write('Acl.database', 'default');
-
-DispatcherFactory::add('Routing');
-DispatcherFactory::add('ControllerFactory');
--- tests/test_app/tests/TestCase/Controller/PagesControllerTest.php
+++ tests/test_app/tests/TestCase/Controller/PagesControllerTest.php
@@ -14,13 +14,13 @@
  */
 namespace App\Test\TestCase\Controller;
 
+use App\Controller\PagesController;
 use Cake\Core\App;
 use Cake\Core\Configure;
 use Cake\Http\Response;
 use Cake\Http\ServerRequest;
 use Cake\TestSuite\IntegrationTestCase;
 use Cake\View\Exception\MissingTemplateException;
-use PtaApp\Controller\PagesController;
 
 /**
  * PagesControllerTest class
